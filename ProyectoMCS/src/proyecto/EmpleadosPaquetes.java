740f5211637a41bc0d02457147b634d9c7ee9420
Bryan Ramos <bramos6506@uta.edu.ec>
2022-12-28 20:27:58 -0500

    Corregí un error en LoginMain

--- ProyectoMCS/src/proyecto/EmpleadosPaquetes.java
+++ ProyectoMCS/src/proyecto/EmpleadosPaquetes.java
@@ -14,7 +14,7 @@ public class EmpleadosPaquetes extends javax.swing.JFrame {
     /**
      * Creates new form EmpleadosPaquetes
      */
-    public EmpleadosPaquetes() {
+    public EmpleadosPaquetes(String cedula) {
         initComponents();
     }
 
