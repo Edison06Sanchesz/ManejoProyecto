428e05c08240acdeeb81840e70ee661a9d16c412
Edison Sanchez <esanchez8526@uta.edu.ec>
2022-12-30 17:10:28 -0500

    Se agrego el metodo guardar

--- ProyectoMCS/src/proyecto/RegistroEmpleado.java
+++ ProyectoMCS/src/proyecto/RegistroEmpleado.java
@@ -65,6 +65,9 @@ public class RegistroEmpleado extends javax.swing.JFrame {
         this.desbloquearBotones();
         this.limpiarTexto();
     }
+    public void Guardar(){
+        
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
