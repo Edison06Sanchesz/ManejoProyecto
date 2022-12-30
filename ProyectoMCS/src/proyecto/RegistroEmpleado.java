d9b073089305af42ab047b8104dda6794a058617
Edison Sanchez <esanchez8526@uta.edu.ec>
2022-12-30 17:31:38 -0500

    se situa el cursos en el jtxtApellido

--- ProyectoMCS/src/proyecto/RegistroEmpleado.java
+++ ProyectoMCS/src/proyecto/RegistroEmpleado.java
@@ -76,6 +76,7 @@ public class RegistroEmpleado extends javax.swing.JFrame {
           jtxtNombre.requestFocus();
         }else if (jtxtApellido.getText().isEmpty()){
             JOptionPane.showMessageDialog(null, "Ingrese un Apellido");
+            jtxtApellido.requestFocus();
         }else if(jtxtSalario.getText().isEmpty()){
             JOptionPane.showMessageDialog(null, "Ingrese un salario");
         }else if(jcbxRol.getSelectedItem().toString().equals("")){
