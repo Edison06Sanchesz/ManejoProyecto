e1bae1e053e529623bd004e52415485c96c0e20e
tamimi <tmaliza4821@uta.edu.ec>
2023-01-28 01:02:08 -0500

    Instancié el login en la clase Main del proyecto

--- ProyectoMCS/src/proyecto/MainProyecto.java
+++ ProyectoMCS/src/proyecto/MainProyecto.java
@@ -15,7 +15,8 @@ public class MainProyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LoginMain dato=new LoginMain();
+        dato.setVisible(true);
     }
     
 }
